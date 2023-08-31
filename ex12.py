75b57603ce30af2eaa7c56f398d65c1489a65554
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-30 21:21:00 -0400

    added notes

--- ex12.py
+++ ex12.py
@@ -1,5 +1,7 @@
+#These 3 lines ask the user to enter someting. The specifc info is asked tbrough adding (). Var stores info to be used later
 age = input ("How old are you? ")
 height = input ("How tall are you? ")
 weight = input ("How much do you weigh? ")
 
+#This is an F string and formats all the inputs defined from previous Var onto a single line allowing them to print as an output
 print (f"So, you're {age} old, {height} tall and {weight} heavy.")
